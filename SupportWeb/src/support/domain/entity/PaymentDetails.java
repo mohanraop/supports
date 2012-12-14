9af79c51f67939c45f2ff58d6b111dd40ad7cf58
mohanrao360 <mohanrao360@7571df8c-574b-26ec-c669-f4fa803635e8>
2012-12-14 04:49:08 +0000

    payment

---
+++ SupportWeb/src/support/domain/entity/PaymentDetails.java
@@ -0,0 +1,224 @@
+package support.domain.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity(name = "SUP_PAYMENT")
+public class PaymentDetails {
+	@Id
+	@GenericGenerator(name = "idGenerator", strategy = "native")
+	@GeneratedValue(generator = "idGenerator")
+	@Column(name = "SUP_PAYMENT_ID")
+	private String id;
+	
+	@Column(name = "CONTACT_ID")
+	private Integer memberId;
+	
+	@Column(name = "SUP_ACCOUNT_ID")
+	private Integer accountId;
+	
+	@Column(name = "PAYMENT_DATE")
+	private Date paymentDate;
+	
+	@Column(name = "PAYMENT_TYPE")
+	private String paymentType;
+	
+	@Column(name = "AMMOUNT")
+	private Double ammount;
+	
+	@Column(name = "CURRENCY")
+	private String currency;
+
+	@Column(name = "CURRENCY_RATE")
+	private Double currencyRate;
+	
+	@Column(name = "START_DT")
+	private Date startDate;
+
+	@Column(name = "END_DT")
+	private Date endDate;
+	
+	@Column(name = "CREATE_DT")
+	private Date createDateTime;
+
+	@Column(name = "UPDATE_DT")
+	private Date updateDateTime;
+
+	/**
+	 * @return the startDate
+	 */
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	/**
+	 * @param startDate the startDate to set
+	 */
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	/**
+	 * @return the endDate
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	/**
+	 * @return the createDateTime
+	 */
+	public Date getCreateDateTime() {
+		return createDateTime;
+	}
+
+	/**
+	 * @param createDateTime the createDateTime to set
+	 */
+	public void setCreateDateTime(Date createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	/**
+	 * @return the updateDateTime
+	 */
+	public Date getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	/**
+	 * @param updateDateTime the updateDateTime to set
+	 */
+	public void setUpdateDateTime(Date updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the memberId
+	 */
+	public Integer getMemberId() {
+		return memberId;
+	}
+
+	/**
+	 * @param memberId the memberId to set
+	 */
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+
+	/**
+	 * @return the accountId
+	 */
+	public Integer getAccountId() {
+		return accountId;
+	}
+
+	/**
+	 * @param accountId the accountId to set
+	 */
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+
+	/**
+	 * @return the paymentDate
+	 */
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+
+	/**
+	 * @param paymentDate the paymentDate to set
+	 */
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	/**
+	 * @return the paymentType
+	 */
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	/**
+	 * @param paymentType the paymentType to set
+	 */
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	/**
+	 * @return the ammount
+	 */
+	public Double getAmmount() {
+		return ammount;
+	}
+
+	/**
+	 * @param ammount the ammount to set
+	 */
+	public void setAmmount(Double ammount) {
+		this.ammount = ammount;
+	}
+
+	/**
+	 * @return the currency
+	 */
+	public String getCurrency() {
+		return currency;
+	}
+
+	/**
+	 * @param currency the currency to set
+	 */
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	/**
+	 * @return the currencyRate
+	 */
+	public Double getCurrencyRate() {
+		return currencyRate;
+	}
+
+	/**
+	 * @param currencyRate the currencyRate to set
+	 */
+	public void setCurrencyRate(Double currencyRate) {
+		this.currencyRate = currencyRate;
+	}
+	
+	
+	
+	
+}
