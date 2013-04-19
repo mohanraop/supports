ee3c65bb62932130814741fbf77bc4a605e4c2e8
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2013-04-19 22:57:04 +0000

    Training

---
+++ SupportWeb/src/support/training/hibernate/main/MainUser.java
@@ -0,0 +1,39 @@
+package support.training.hibernate.main;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+import support.training.hibernate.bean.User;
+
+
+public class MainUser {
+	public static void main(String[] args) {
+		
+			
+			try{
+				SessionFactory sessionFactory = new Configuration().configure("/hibernate.cfg.xml").buildSessionFactory();
+				Session session =  sessionFactory.getCurrentSession();
+				
+				Transaction transaction =  session.beginTransaction();
+				
+				
+				User user = new User();
+				user.setPlace("GUN");
+				
+				session.saveOrUpdate(user);
+				
+							
+				transaction.commit();
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+}
+
