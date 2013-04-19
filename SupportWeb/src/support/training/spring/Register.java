408623400d870e65fe7a2c8492330c95341798a4
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2013-04-19 20:52:33 +0000

    Training

--- SupportWeb/src/support/training/spring/Register.java
+++ SupportWeb/src/support/training/spring/Register.java
@@ -6,15 +6,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Register {
 	
-	@Resource(name="student")
-	Student student;
-	
 	public static void main(String[] args) {
 		ApplicationContext context= new ClassPathXmlApplicationContext("/applicationTraining.xml");
-		Student s1= (Student) context.getBean("student");
-		s1.setSname("madhu");
-		s1.setSno(37);
-		s1.result();
+		Student student= (Student) context.getBean("student");
+		student.result();
+		PList plist=(PList) context.getBean("plist");
+		plist.values();
 	}
 
 }
