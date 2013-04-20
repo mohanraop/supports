ece1bf0792fba01e0ea605e802c2278e0d95e476
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2013-04-20 04:59:35 +0000

    Training

--- ShareTradingSystem/src/com/train/ws/service/ShareInfromation.java
+++ ShareTradingSystem/src/com/train/ws/service/ShareInfromation.java
@@ -6,30 +6,42 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ShareInfromation {
-	
+
 	Connection connect = null;
-	 
-	 Statement stmt1= null;
-	
-	private void createConnection(){
+
+	Statement stmt = null;
+
+	private void createConnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");	
+			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("1.Driver is loaded");
-		} catch(java.lang.ClassNotFoundException e) {
+		} catch (java.lang.ClassNotFoundException e) {
 			System.err.print("com.mysql.jdbc.Driver");
 			System.err.println(e.getMessage());
 		}
-		 try {
-			   connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbeqp", "root", "root");
-			   System.out.println("2.Connection established");
-			} catch(SQLException ex) {
-				System.err.println("SQLException: " + ex.getMessage());
-			}
+		try {
+			connect = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/dbeqp", "root", "root");
+			System.out.println("2.Connection established");
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
 
 	}
-	public void insertShare(String shareName,int sharePrice,String shareShortName){
+
+	public void insertShare(String shareName, int sharePrice) {
 		createConnection();
-		
-	}
+		String s1;
+		s1 = "insert into stock values(" + sharePrice + ",'" + shareName + "')";
 
+		try {
+			stmt = connect.createStatement();
+			stmt.executeUpdate(s1);
+			stmt.close();
+			connect.close();
+
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+	}
 }
