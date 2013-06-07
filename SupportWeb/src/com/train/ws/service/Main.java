c49eea51ffeca683d9cb0fa13cb8d1d69a35b644
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2013-06-07 04:03:28 +0000

    conflict

--- SupportWeb/src/com/train/ws/service/Main.java
+++ SupportWeb/src/com/train/ws/service/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) throws RemoteException {
 		CurrencyProxy currencyProxy = new CurrencyProxy();
 		System.out.println(currencyProxy.getCountry("India"));
-		int i=10;
+		int i=15;
 		ShareInfromationProxy shareInfromationProxy = new ShareInfromationProxy();
 		shareInfromationProxy.insertShare("TCS", 1500);
 	}
