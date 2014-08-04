6bbea648b0743adf9f7b89dc78349f0a079eb9fe
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2014-08-04 02:09:29 +0000

    throw

--- Example/src/com/exp/cn/exc/Excep.java
+++ Example/src/com/exp/cn/exc/Excep.java
@@ -5,60 +5,57 @@ package com.exp.cn.exc;
 
 /**
  * @author Amar
- *
+ * 
  */
 public class Excep {
-	
-	
-	
-	
-	public void m1() throws Exception{
+
+	public void m1() throws Exception {
 		System.out.println("m1-begin");
 		m2();
 		System.out.println("m1-end");
 	}
-	
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		Excep excep = new Excep();
-		
+
 		try {
 			excep.m1();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//throw
-		
-		/*try {
-			int i =1/0;
-			System.out.println(i);
-			System.out.println(12345);
-		}catch (ArithmeticException e) {
-			System.out.println("-----ArithmeticException");
-		}catch (IndexOutOfBoundsException e) {
-			System.out.println("-----IndexOutOfBoundsException");
-		}catch (Exception e) {
-			System.out.println("-----Exception");
-		}*/
-		
+
+		// throw
+
+		/*
+		 * try { int i =1/0; System.out.println(i); System.out.println(12345);
+		 * }catch (ArithmeticException e) {
+		 * System.out.println("-----ArithmeticException"); }catch
+		 * (IndexOutOfBoundsException e) {
+		 * System.out.println("-----IndexOutOfBoundsException"); }catch
+		 * (Exception e) { System.out.println("-----Exception"); }
+		 */
+
 		System.out.println(1);
-		System.out.println(0);		
+		System.out.println(0);
 		System.out.println(2);
 	}
-	
-	
 
 	public void m2() throws Exception {
-		
+
 		System.out.println("m2");
-		int j=1/0;
-		System.out.println(j);
+		try {
+			int j = 1 / 0;
+			System.out.println(j);
+		} catch (ArithmeticException ae) {
+			System.out.println("partial exception handling");
+			//throw ae;
+			throw new Exception("Please handle");
+		}
 	}
-	
 
 }
