41591649618b3aae828950870ad99f4e2efa5c08
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2015-03-06 16:24:06 +0000

    initial

---
+++ SpringMaven/src/main/java/com/spring/bean/EmployeeBean.java
@@ -0,0 +1,17 @@
+package com.spring.bean;
+
+public class EmployeeBean 
+{
+	String empName;
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	
+
+}
