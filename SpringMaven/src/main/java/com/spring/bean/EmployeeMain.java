06230b7aac2fcd12330482e41694425ae86a4dd7
mohanrao360@gmail.com <mohanrao360@gmail.com@7571df8c-574b-26ec-c669-f4fa803635e8>
2015-03-20 02:01:49 +0000

    final

--- SpringMaven/src/main/java/com/spring/bean/EmployeeMain.java
+++ SpringMaven/src/main/java/com/spring/bean/EmployeeMain.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("employeeContext.xml");
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//EmployeeBean eb = new EmployeeBean("abcd");
 		EmployeeBean eb =(EmployeeBean) ac.getBean("emp");
 		System.out.println(eb.getEmpName());
